d178d8e2b1f7715da81c1f98ad9a5a24335a952c
Johan <johan.a.publ@mailbox.org>
2026-07-20 21:38:06 +0200

    Fixed /api routing to BE with PathPrefix, changed FE adresses and re-enabled CORS in BE.

--- kptn-app1-backend/src/main.go
+++ kptn-app1-backend/src/main.go
@@ -1,50 +1,45 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "strconv"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 // Define build variables here. Variables are set at build time with '-X main.<variable>'
 var version string
 
 func main() {
-    visitors := 0
-    
-    // Print the build version
-    fmt.Println("App Version:", version)
-    
-    // Define endpoints
-    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        // Allow all origins (use specific domain in production)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	
-	// Handle request
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
-    })
-    
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    	visitors++
-    	visitorsStr := strconv.Itoa(visitors)
-    	
-    	// Allow all origins (use specific domain in production)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	
-	// Handle request
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(map[string]string{"visitors": visitorsStr})
-    })
- 
-    // Start server
-    fmt.Println("Server running on :80")
-    log.Fatal( http.ListenAndServe(":80", nil) )
-}
+	visitors := 0
 
+	// Print the build version
+	fmt.Println("App Version:", version)
+
+	// Define endpoints
+	//   All endpoints must start with '/api' for the routing setup to work and to be able to
+	//   publish the API on the same root-address as the frontend to comply with CORS.
+	http.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		visitors++
+		visitorsStr := strconv.Itoa(visitors)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"visitors": visitorsStr})
+	})
+
+	// test
+	http.HandleFunc("/api/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"test": "api/test"})
+	})
+
+	// Start server
+	fmt.Println("Server running on :80")
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
